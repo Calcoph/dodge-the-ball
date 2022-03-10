b10661772000dfe0916e509a94be90a83f1b04a2
calcopiritus <34724728+Calcoph@users.noreply.github.com>
2022-03-10 12:45:35 +0100

    dodgers don't go through floor

--- src/dodger.rs
+++ src/dodger.rs
@@ -26,9 +26,9 @@ impl Dodger
 
     pub fn move_tick(&mut self) {
         let mut new_y = self.y + self.speed;
-        if new_y >= CEILING_HEIGHT
+        if new_y+self.height >= CEILING_HEIGHT
         {
-            new_y = CEILING_HEIGHT
+            new_y = CEILING_HEIGHT-self.height
         } else if new_y <= 0.0
         {
             new_y = 0.0
